d049e612e14a9595f50d866c60c2d424d2663d8f
sukritihere <sukritisingh9469@gmail.com>
2025-01-18 21:13:42 +0530

    feat: fix default dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="flex h-screen">
+      <div className="flex h-screen ">
         <Sidebar />
         <div className="flex-1 flex flex-col">
           <Header />
